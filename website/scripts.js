71eb313b95d5ccad65376713a9cb553a04c3b1e5
Arne Z <10495800@student.uva.nl>
2017-03-04 15:35:28 +0100

    started with reading storage entries

--- website/scripts.js
+++ website/scripts.js
@@ -11,3 +11,41 @@ function addToSessionStorage() {
 	var value = document.getElementById("storage_value").value;
 	sessionStorage.setItem(key, value);
 }
+
+function updateTables(storageType) {
+	var newTbody = document.createElement("tbody");
+
+	if (storageType === "local") {
+		var tbody = document.getElementById("local-tbody");
+	}
+	else if (storageType === "session") {
+		var tbody = document.getElementById("session-tbody");
+	}
+	else {
+		console.log("unknown storage type: " + storageType);
+	}
+	tbody.parentNode.replaceChild(newTbody, tbody);
+}
+
+function populateTbody(storageType) {
+	var storage;
+	if (storageType === "local") {
+		storage = localStorage;
+	}
+	else if (storageType === "session") {
+		storage = sessionStorage;
+	}
+	else {
+		console.log("unknown storage type: " + storageType);
+	}
+
+	// loop over storage entries
+	for (var property in storage) {
+		if (storage.hasOwnProperty(property)) {
+			console.log(property + ": " + storage[property]);
+		}
+	}
+	return true;
+
+
+}
